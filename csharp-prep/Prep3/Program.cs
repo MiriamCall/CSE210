75dfe0711560e4eabe3a6a5a24b3ae2b806c31cc
MiriamCall <bow18005@byui.edu>
2024-06-11 13:20:50 -0600

    chore: Refactor program to implement a number guessing game
    The program has been refactored to implement a number guessing game. Instead of simply printing a greeting message, the program now generates a random number and prompts the user to guess it. The user's guess is compared to the generated number, and appropriate feedback is provided. The game continues until the user guesses the correct number. This change improves the functionality and interactivity of the program.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        // Random number generator
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // User picks magic number
+        // Console.WriteLine("What is the magic number?");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = 0;
+        
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (magicNumber > guess)
+            {Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess){
+                Console.WriteLine("Lower");
+
+            }
+            else{
+                Console.WriteLine("You Guessed it!");
+            }
+
+        }
     }
 }
