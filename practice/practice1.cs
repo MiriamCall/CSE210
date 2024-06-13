df3bd560751226d5fe18f606c47754073549f19c
MiriamCall <bow18005@byui.edu>
2024-06-13 12:19:17 -0600

    practice

---
+++ practice/practice1.cs
@@ -0,0 +1,43 @@
+
+using System;
+
+class Program{
+
+
+static void Main(string[] args){
+    // int userAge = -1;
+
+    // while (userAge < 0 || userAge > 120)
+    // {
+    //     Console.WriteLine("Please input your age (0-120): ");
+    //     userAge = int.Parse(Console.ReadLine());
+    // }
+
+    // int userAge2;
+
+    // do
+    // {
+    //     Console.WriteLine("Please input your age (0-120): ");
+    //     userAge = int.Parse(Console.ReadLine());
+    // } while (userAge < 0 || userAge > 120);
+
+
+
+    for(int i = 0; i < 100; i++){
+        Console.WriteLine("" + i);
+    }
+
+    List<int> myList = new List<int> {1, 2, 3, 4, 5};
+    myList<string> myName = new List<string>();
+
+    myNames.Add("John");
+    myNames.Add("Paul");
+    myNames.Add("George");
+
+    myNumbers.Add(12);
+
+    foreach(string name in myNames){
+        Console.WriteLine("" + name);
+    }
+}
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
+    static double CalculateCircleArea(double radius)
+{
+    double area = 0;
+    area = Math.PI * radius * radius;
+    return area;
+   
+}
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        double area = CalculateCircleArea(10);
+        Console.WriteLine(area);
+
+        
+        
     }
+
+
+
 }
