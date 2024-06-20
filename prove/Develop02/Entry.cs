cedd1bb0a08b0d7e5815e35964cad524db8a0572
MiriamCall <bow18005@byui.edu>
2024-06-19 20:21:03 -0600

    chore: Add Entry and Journal classes
    The Entry and Journal classes have been added to the project. The Entry class represents a journal entry with a question, response, and date. The Journal class manages a collection of entries and provides methods for retrieving, setting, displaying, saving, and loading entries. These classes will enable the creation and management of a journal in the program.

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,30 @@
+using System;
+class Entry 
+{
+    private string _question;
+    private string _response;
+
+    private DateTime _date;
+    public void DisplayEntry()
+    {
+
+    }
+    public DateTime GetDate(DateTime date)
+    {
+        return date;
+    }
+
+    public void SetDate()
+    {
+        
+    }
+    public string GetResponse(string response)
+    {
+        return response;
+    }
+    public void SetResponse()
+    {
+
+    }
+
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,27 @@
+
+class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+public Entry GetEntry (Entry entry)
+{
+    return entry;
+}
+public void SetEntry()
+{
+
+}
+public void DisplayEntries()
+{
+
+}
+public void SaveJournal()
+{
+
+}
+public void LoadJournal()
+{
+
+}
+
+}
