964d0d4c373d16afa356141377498c22a9c64020
MiriamCall <bow18005@byui.edu>
2024-06-24 20:20:26 -0600

    feat: Add fraction class with basic functionality

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        fraction f1 = new fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        fraction f2 = new fraction(5)
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        fraction f3 = new fraction(6, 7)
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -1,11 +1,54 @@
-fraction()
-fraction(wholeNumber : int)
-fraction(typeof : int, bottom : int)
+public class fraction
+{
+    private int _top;
+    private int _bottom;
 
-GetTop()
-SetTop(typeof : int)
-GetBottom()
-SetBottom(bottom : int)
+    public fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
 
-GetFractionString() : string
-GetDecimalValue() : double
+    public fraction(wholeNumber : int)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public fraction(typeof : int, bottom : int)
+    {
+        _top = typeof;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(typeof : int)
+    {
+        _top = typeof;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(bottom : int)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return _top + "/" + _bottom;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
