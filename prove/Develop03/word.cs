feda8f690ab94b5dcb269832a99ac7dee0e87219
MiriamCall <bow18005@byui.edu>
2024-06-27 14:32:19 -0600

    Refactor code in Develop03, reference, and scripture classes

--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -11,13 +11,30 @@ class Word
 
     public void DisplayWord() 
     {
+       
         Console.Write(_word);
+        
+        
     }
 
     public void Hide()
     {
         _isVisible = false;
     }
+
+    // public void HideThreeWords()
+    // {
+    //     Random random = _words.Where(Word => Word.IsVisible).ToList();
+
+    //     for (int i = 0; i < Math.Min(3, visibleWords.Count); i++)
+    //     {
+    //         int indexToHide = random.Next(visibleWords.Count);
+    //         visibleWords[indexToHide].ToggleVisiblility();
+    //         visibleWords.RemoveAt(indexToHide);
+    //     }
+    // }
+
+
     // public override string ToString()
     // {
     //     if (_isVisible)
